b378ab815c6fc2c5508bbff22b8cacc60dc8d8f0
Isabella Pacheco <isabellapacheco404@gmail.com>
2023-09-14 16:58:06 -0300

    javascript add

--- pacifico.js
+++ pacifico.js
@@ -1,4 +1,24 @@
+function exibe_text(elementId) {
+    document.getElementById(elementId).style.visibility = "visible";
+}
 
+function some_tudo(elementId) {
+    document.getElementById(elementId).style.visibility = "hidden";
+}
 
+for (let i = 1; i <= 5; i++) {
+    const elementId = `element${i}`;
+    const textId = `text${i}`;
+    
+    const element = document.getElementById(elementId);
+    
+    element.addEventListener("mouseover", () => {
+        exibe_text(textId);
+    });
+    
+    element.addEventListener("mouseout", () => {
+        some_tudo(textId);
+    });
+}
     
 
