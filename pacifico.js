0089bf5fa506548ff4d15c0a65813bb17e69a242
Isabella Pacheco <isabellapacheco404@gmail.com>
2023-09-14 19:56:36 -0300

    .

--- pacifico.js
+++ pacifico.js
@@ -20,5 +20,21 @@ for (let i = 1; i <= 5; i++) {
         some_tudo(textId);
     });
 }
-    
+
+var ocean = document.getElementById("ocean"),
+    waveWidth = 10,
+    waveCount = Math.floor(window.innerWidth/waveWidth),
+    docFrag = document.createDocumentFragment();
+
+for(var i = 0; i < waveCount; i++){
+  var wave = document.createElement("div");
+  wave.className += " wave";
+  docFrag.appendChild(wave);
+  wave.style.left = i * waveWidth + "px";
+  wave.style.webkitAnimationDelay = (i/100) + "s";
+}
+
+ocean.appendChild(docFrag);
+
+AOS.init();
 
