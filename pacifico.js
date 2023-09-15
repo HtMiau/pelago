39aa71e4b556fc4726fb26f42b41439ba94fcf58
Isabella Pacheco <isabellapacheco404@gmail.com>
2023-09-15 13:50:42 -0300

    adicionando mais imagens

--- pacifico.js
+++ pacifico.js
@@ -21,24 +21,6 @@ for (let i = 1; i <= 10; i++) {
     });
 }
 
-// make some waves.
-var ocean = document.getElementById("ocean"),
-    waveWidth = 10,
-    waveCount = Math.floor(window.innerWidth/waveWidth),
-    docFrag = document.createDocumentFragment();
-
-for(var i = 0; i < waveCount; i++){
-  var wave = document.createElement("div");
-  wave.className += " wave";
-  docFrag.appendChild(wave);
-  wave.style.left = i * waveWidth + "px";
-  wave.style.webkitAnimationDelay = (i/100) + "s";
-}
-
-ocean.appendChild(docFrag);
-
-
-
 
 AOS.init();
 
