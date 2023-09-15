5ca05c165520317fb2ff6d7b96651048dc432740
HtMiau <davig4560@gmail.com>
2023-09-14 22:22:24 -0300

    alteração 7

---
+++ antartico.js
@@ -0,0 +1,47 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('js-player', {
+        height: '390',
+        width: '640',
+        videoId: '-JJAXwAaA2w',
+		controls: 0,
+		modestbranding: 1,
+		rel: 0,
+		showinfo: 0,
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function pauseVideo() {
+    player.pauseVideo();
+}
+
+function playVideo() {
+    player.playVideo();
+}
+
+var playing = true;
+
+document.getElementById('js-video-container').addEventListener('click', function (e) {
+
+    if(playing == true) {
+        pauseVideo();
+        playing = false;
+    } else {
+        playVideo();
+        playing = true;
+    }
+  
+});
