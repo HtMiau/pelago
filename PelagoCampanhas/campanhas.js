41a148b77ab20049a7fcb90db35f205b8221482f
Matheus José <matheusjose1242@gmail.com>
2023-09-17 19:31:07 -0300

    pastas com arquivos adicionados

---
+++ PelagoCampanhas/campanhas.js
@@ -0,0 +1,20 @@
+
+window.sr = ScrollReveal({reset:true});
+
+
+sr.reveal('.titulo_algas', {rotate:{ x:100, y:0, z:0}, duration : 2000});
+
+sr.reveal('.corpo_texto', {rotate:{ x:100, y:0, z:0}, duration : 2000});
+
+sr.reveal('.corpo_imagem', {rotate:{ x:100, y:0, z:0}, duration : 2000});
+
+sr.reveal('.corpo_imagem2', {rotate:{ x:100, y:0, z:0}, duration : 2000});
+
+sr.reveal('.corpo_texto2', {rotate:{ x:100, y:0, z:0}, duration : 2000});
+
+sr.reveal('.audio_align', {rotate:{ x:100, y:0, z:0}, duration : 2000});
+
+sr.reveal('.videoefeito', {rotate:{ x:100, y:0, z:0}, duration : 2000});
+
+
+
