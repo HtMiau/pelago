36faa23b6da216d96b44b950e7fa610b3ce48b36
Davidjr99 <davidjunior3010@gmail.com>
2023-09-18 17:15:46 -0300

    .

--- antartico.js
+++ antartico.js
@@ -1,33 +1,3 @@
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('js-player', {
-        height: '390',
-        width: '640',
-        videoId: '-JJAXwAaA2w',
-		controls: 0,
-		modestbranding: 1,
-		rel: 0,
-		showinfo: 0,
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-}
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-function pauseVideo() {
-    player.pauseVideo();
-}
-
 function exibe_text(elementId) {
     document.getElementById(elementId).style.visibility = "visible";
 }
