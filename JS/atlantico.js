4235851732f03cbe64194a5b5737de23ee04a226
Isabella Pacheco <isabellapacheco404@gmail.com>
2023-09-20 19:35:41 -0300

    erro do f12

--- JS/atlantico.js
+++ JS/atlantico.js
@@ -1,25 +1,26 @@
-function exibe_text(elementId) {
-    document.getElementById(elementId).style.visibility = "visible";
-}
+document.addEventListener('DOMContentLoaded', function () {
+    function exibe_text(elementId) {
+        document.getElementById(elementId).style.visibility = "visible";
+    }
 
-function some_tudo(elementId) {
-    document.getElementById(elementId).style.visibility = "hidden";
-}
+    function some_tudo(elementId) {
+        document.getElementById(elementId).style.visibility = "hidden";
+    }
 
-for (let k = 1; k <= 10; k++) {
-    const elementId = `element${k}`;
-    const textId = `text${k}`;
-    
-    const element = document.getElementById(elementId);
-    
-    element.addEventListener("mouseover", () => {
-        exibe_text(textId);
-    });
-    
-    element.addEventListener("mouseout", () => {
-        some_tudo(textId);
-    });
-}
+    for (let k = 1; k <= 10; k++) {
+        const elementId = `element${k}`;
+        const textId = `text${k}`;
+        
+        const element = document.getElementById(elementId);
+        
+        element.addEventListener("mouseover", () => {
+            exibe_text(textId);
+        });
+        
+        element.addEventListener("mouseout", () => {
+            some_tudo(textId);
+        });
+    }
 
-
-AOS.init();
+    AOS.init();
+});
